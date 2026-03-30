80a4e4bfdd365422a51c91271757d3699c3a6a8f
yukari6559 <touhoudaisuki2019@gmail.com>
2026-03-30 23:13:45 +0900

    complete-cpu

---
+++ 06_cpu/decoder.vhd
@@ -0,0 +1,46 @@
+-- decoder.vhd
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity decoder is
+	port (
+		instr	: in std_logic_vector(31 downto 0);
+		rs1		: out std_logic_vector(4 downto 0);
+		rs2		: out std_logic_vector(4 downto 0);
+		rd		: out std_logic_vector(4 downto 0);
+		imm		: out std_logic_vector(31 downto 0);
+		alu_op	: out std_logic_vector(3 downto 0);
+		reg_we	: out std_logic;
+		mem_we	: out std_logic
+	);
+end entity;
+
+architecture rtl of decoder is
+	signal opcode : std_logic_vector(6 downto 0);
+begin
+	rs1 <= instr(19 downto 15);
+	rs2 <= instr(24 downto 20);
+	rd  <= instr(11 downto  7);
+	opcode <= instr(6 downto 0);
+	process(instr)
+	begin
+		case instr(6 downto 0) is
+			when "0110011" => -- R型（add）
+				alu_op <= "0000";
+				reg_we <= '1';
+				mem_we <= '0';
+				imm	<= (others => '0');
+			when "0010011" => -- I型（addi）
+				alu_op <= "0000";
+				reg_we <= '1';
+				mem_we <= '0';
+				imm <= std_logic_vector(resize(signed(instr(31 downto 20)), 32));
+			when others =>
+				alu_op <= "0000";
+				reg_we <= '0';
+				mem_we <= '0';
+				imm <= (others => '0');
+		end case;
+	end process;
+end architecture;
---
+++ 06_cpu/tb_cpu_top.vhd
@@ -0,0 +1,34 @@
+-- tb_cpu_top.vhd
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity tb_cpu_top is
+end entity;
+
+architecture sim of tb_cpu_top is
+	signal clk		: std_logic;
+	signal rst		: std_logic;
+	signal instr	: std_logic_vector(31 downto 0);
+	signal pc		: std_logic_vector(31 downto 0);
+begin
+	uut: entity work.cpu_top 
+		port map(
+			clk => clk,
+			rst => rst,
+			instr => instr,
+			pc => pc
+		);
+	process
+	begin
+		rst <= '1'; wait for 5 ns;
+		rst <= '0'; wait for 5 ns;
+		instr <= x"003100B3"; wait for 5 ns;
+		instr <= x"00510093"; wait for 5 ns;
+		wait;
+	end process;
+	process
+	begin
+		clk <= '0'; wait for 5 ns;
+		clk <= '1'; wait for 5 ns;
+	end process;
+end architecture;
